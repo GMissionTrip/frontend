40a840478bfb654828df86add681c308b1a2bb35
minseok <alstjrwjd99@naver.com>
2025-07-06 10:41:46 +0900

    feat: 랜딩페이지 UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
 
 function App() {
-	return (
-		<Routes>
-			{/* <Route path="/" element={<Home />} />
-			<Route path="/about" element={<About />} /> */}
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/about" element={<About />} /> */}
+    </Routes>
+  );
 }
 
 export default App;
