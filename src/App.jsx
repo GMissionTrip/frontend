72b41cbd0adbeca2a4d52620713f4c4e311d15c8
minseok <alstjrwjd99@naver.com>
2025-07-06 11:59:08 +0900

    feat: 기본 레이아웃 & 로그인 UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/about" element={<About />} /> */}
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
